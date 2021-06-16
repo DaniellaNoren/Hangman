ec0381e1a6d2f70eb81943de7d126320d2a02631
Daniella Noren <daniellanoren@live.se>
2021-06-16 10:14:44 +0200

    Add StringMethodsTest and WordImporterTest

---
+++ Hangman.test/StringMethodsTests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Hangman.test
+{
+    public class StringMethodsTests
+    {
+        [Fact]
+        public void GetCharArrayWithChar_ShouldMatchLengthAndChar()
+        {
+            char[] charArray = StringMethods.GetCharArrayWithChar(10, 't');
+
+            Assert.True(charArray.Length == 10);
+            foreach (char c in charArray)
+            {
+                Assert.True(c == 't');
+            }
+        }
+
+        [Fact]
+        public void GetCharArrayWithChar_ShouldMatchDefaultChar()
+        {
+            char[] charArray = StringMethods.GetCharArrayWithChar(5);
+
+            foreach (char c in charArray)
+            {
+                Assert.True(c == '_');
+            }
+        }
+
+        [Theory]
+        [InlineData(0, true)]
+        [InlineData(10, true)]
+        [InlineData(1, true)]
+        public void GetCharArrayWithChar_ShouldMatchLength(int length, bool expected)
+        {
+            char[] charArray = StringMethods.GetCharArrayWithChar(length);
+
+            Assert.Equal(charArray.Length == length, expected);
+        }
+
+        [Theory]
+        [InlineData('s', true)]
+        [InlineData('K', true)]
+        [InlineData('k', true)]
+        [InlineData('ö', false)]
+        [InlineData(' ', false)]
+        public void StringBuilderContainsLetter_ShouldReturnTrueOrFalse(char letter, bool expected)
+        {
+            StringBuilder exampleSentence = new StringBuilder("Kokosbollar");
+
+            Assert.Equal(StringMethods.StringBuilderContainsLetter(exampleSentence, letter), expected);
+            
+        }
+
+        [Fact]
+        public void IndexesOf_ShouldReturnListWithThreeIndexes()
+        {
+            string s = "CHOKLADBULLAR";
+            List<int> indexesList = StringMethods.IndexesOf(s, 'L');
+
+            Assert.True(indexesList.Count == 3);
+            Assert.True(indexesList[0] == 4);
+            Assert.True(indexesList[1] == 9);
+            Assert.True(indexesList[2] == 10);
+
+        }
+
+        [Fact]
+        public void IndexesOf_ShouldReturnListWithOneIndex()
+        {
+            string s = "KOKOSMJÖL";
+            List<int> indexesList = StringMethods.IndexesOf(s, 'Ö');
+
+            Assert.True(indexesList.Count == 1);
+            Assert.True(indexesList[0] == 7);
+
+        }
+
+
+        [Fact]
+        public void IndexesOf_ShouldReturnEmptyList()
+        {
+            string s = "KOKOSMJÖL";
+            List<int> indexesList = StringMethods.IndexesOf(s, 'Y');
+
+            Assert.True(indexesList.Count == 0);
+        }
+
+
+        [Fact]
+        public void SetLetterInIndexes_ShouldSetCharacterAtCorrectIndexes()
+        {
+            char[] charArray = new char[] { 'a', 'b', 'c', 'd', 'e' };
+            List<int> indexList = new List<int>() { 0, 3, 4 };
+            char c = 't';
+
+            StringMethods.SetLetterInIndexes(indexList, ref charArray, c);
+
+            Assert.True(charArray[indexList[0]] == c);
+            Assert.True(charArray[indexList[1]] == c);
+            Assert.True(charArray[indexList[2]] == c);
+            Assert.True(charArray[2] != c);
+
+        }
+
+         [Fact]
+        public void SetLetterInIndexes_ShouldSetCharacterAtCorrectIndex()
+        {
+            char[] charArray = new char[] { 'a', 'b', 'c', 'd', 'e' };
+            List<int> indexList = new List<int>() { 2 };
+            char c = 't';
+
+            StringMethods.SetLetterInIndexes(indexList, ref charArray, c);
+            Assert.True(charArray[indexList[0]] == c);
+            Assert.True(charArray[1] != c);
+        }
+
+        [Theory]
+        [InlineData("hello", true)]
+        [InlineData("he llo", false)]
+        [InlineData("goodbye", false)]
+        public void StringMatchesCharArray_ShouldReturnTrueOrFalse(string s, bool expected)
+        {
+            char[] charArray = new char[] { 'h', 'e', 'l', 'l', 'o' };
+
+            Assert.Equal(StringMethods.StringMatchesCharArray(s, charArray), expected);
+        }
+
+
+
+    }
+}
---
+++ Hangman.test/WordImporterTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Hangman.test
+{
+    public class WordImporterTests
+    {
+
+        [Fact]
+        public void WordImporter_TestConstructor()
+        {
+            string path = "PathToFile";
+            WordImporter wordImporter = new WordImporter(path);
+            Assert.Equal(wordImporter.PathToFile, path);
+        }
+
+        private string FilePath = @"./TestFiles/Words.txt";
+
+        [Fact]
+        public void ImportWordsFromFile_ShouldFindFile()
+        {
+            WordImporter wordImporter = new WordImporter(FilePath);
+            List<string> importedWords = wordImporter.ImportWordsFromFile();
+            Assert.True(importedWords.Count == 3);
+            Assert.Equal("TEST1", importedWords[0]);
+            Assert.Equal("TEST2", importedWords[1]);
+            Assert.Equal("TEST3", importedWords[2]);
+
+        }
+
+        [Fact]
+        public void ImportWordsFromFile_ShouldThrowFileNotFoundException()
+        {
+            WordImporter wordImporter = new WordImporter("wrongpath");
+
+            Assert.Throws<FileNotFoundException>(() => wordImporter.ImportWordsFromFile());
+            Assert.Null(wordImporter.Words);
+
+        }
+
+        [Fact]
+        public void GetOneWord_ThrowExceptionWhenWordsAreNotYetImported()
+        {
+            Assert.Throws<ArgumentNullException>(() => new WordImporter("").GetOneWord());
+        }
+
+        [Fact]
+        public void GetOneWord_GetARandomWordFromImportedWords()
+        {
+            WordImporter wordImporter = new WordImporter(FilePath);
+            List<string> importedWords = wordImporter.ImportWordsFromFile();
+            
+            string randomWord = wordImporter.GetOneWord();
+            Assert.True(randomWord.Equals("TEST1") || randomWord.Equals("TEST2") || randomWord.Equals("TEST3"));
+            randomWord = wordImporter.GetOneWord();
+            Assert.True(randomWord.Equals("TEST1") || randomWord.Equals("TEST2") || randomWord.Equals("TEST3"));
+            randomWord = wordImporter.GetOneWord();
+            Assert.True(randomWord.Equals("TEST1") || randomWord.Equals("TEST2") || randomWord.Equals("TEST3"));
+
+            Assert.True(importedWords.Count == 3);
+         
+        }
+
+        [Fact]
+        public void SetWordsManually_ShouldSetList()
+        {
+            WordImporter wordImporter = new WordImporter("");
+            string[] words = new string[] { "WORD1", "WORD2", "WORD3" };
+
+            wordImporter.SetWordsManually(words);
+
+            Assert.Equal(words.Length, wordImporter.Words.Count);
+            Assert.Equal(words[0], wordImporter.Words[0]);
+            Assert.Equal(words[1], wordImporter.Words[1]);
+            Assert.Equal(words[2], wordImporter.Words[2]);
+
+        }
+
+    }
+}
